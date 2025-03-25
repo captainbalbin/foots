126739dda09ff9ae98513635163f4ab94311f2c5
captainbalbin <albin@sjostrand.dev>
2025-03-25 19:31:58 +0100

    Add basic api

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,46 @@
-import { createClient } from '@supabase/supabase-js'
+import { Loader2 } from 'lucide-react'
 import { Button } from './components/ui/button'
+import { useQuery } from '@tanstack/react-query'
+
+type Team = {
+  id: string
+  name: string
+  createdAt: string
+}
 
 function App() {
-  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-  const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-  const supabase = createClient(supabaseUrl, supabaseKey)
+  const { data, isPending, error, refetch } = useQuery({
+    queryKey: ['teams'],
+    queryFn: async () => {
+      const apiUrl =
+        process.env.NODE_ENV === 'production'
+          ? 'https://your-production-url.com/api/teams'
+          : 'http://localhost:3000/api/teams'
+      const response = await fetch(apiUrl)
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      return response.json()
+    },
+  })
 
-  const getTeams = async () => {
-    const { data, error } = await supabase.from('Teams').select()
+  if (isPending) {
+    return <div>loading teams query...</div>
+  }
 
-    console.log(data)
-    console.log(error)
+  if (error) {
+    return <div>error happened</div>
   }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button onClick={() => getTeams()}>Click me</Button>
+      <Button disabled={isPending} onClick={() => refetch()}>
+        {isPending && <Loader2 className="animate-spin" />}
+        Refetch data
+      </Button>
+      {data.map((team: Team) => {
+        return <div key={team.id}>{team.name}</div>
+      })}
     </div>
   )
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,42 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { createClient } from '@supabase/supabase-js'
 
 const app = new Hono()
 
-// Simple API Endpoint
-app.get('/', (c) => c.text('Hello, World!'))
+app.use('*', cors())
 
-app.get('/greet/:name', (c) => {
-  const name = c.req.param('name')
-  return c.text(`Hello, ${name}!`)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+app.get('/api/teams', async (c) => {
+  const { data, error } = await supabase.from('Teams').select()
+
+  if (error) {
+    console.error(error.message)
+    return c.json({ error: error.message }, 500)
+  }
+
+  return c.json(data)
 })
 
-// Bun Server
+app.get('api/team/:team', async (c) => {
+  const team = c.req.param('team')
+
+  const { data, error } = await supabase.from('Teams').select().eq('id', team)
+
+  if (error) {
+    console.error(error.message)
+
+    return c.json({ error: error.message }, 500)
+  }
+
+  return c.json(data)
+})
+
 Bun.serve({
   fetch(req) {
-    // Hono API
     return app.fetch(req)
   },
   port: 3000,
