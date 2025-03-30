fbbd6782dc54050ce8d0c16ab64d090b5557e9fd
captainbalbin <albin@sjostrand.dev>
2025-03-30 10:40:37 +0200

    Update nav

--- src/features/navigation/index.tsx
+++ src/features/navigation/index.tsx
@@ -1,16 +1,24 @@
+import { Button } from '@/components/ui/button'
 import { Link } from '@tanstack/react-router'
+import { useState } from 'react'
 
 export const Nav = () => {
+  const [activeLink, setActiveLink] = useState<number>(0)
+
   return (
     <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
+      <Link to="/" onClick={() => setActiveLink(0)}>
+        <Button variant={activeLink === 0 ? 'secondary' : 'ghost'}>Home</Button>
       </Link>
-      <Link to="/teams" className="[&.active]:font-bold">
-        Teams
+      <Link to="/teams" onClick={() => setActiveLink(1)}>
+        <Button variant={activeLink === 1 ? 'secondary' : 'ghost'}>
+          Teams
+        </Button>
       </Link>
-      <Link to="/players" className="[&.active]:font-bold">
-        Players
+      <Link to="/players" onClick={() => setActiveLink(2)}>
+        <Button variant={activeLink === 2 ? 'secondary' : 'ghost'}>
+          Players
+        </Button>
       </Link>
     </div>
   )
